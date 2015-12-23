8e44b647403e29bb504344550ebd0886d722463e
ananabanana <derhunova.anna@gmail.com>
2015-12-23 22:39:38 +0100

    new
    new

--- d2g_sandbox/js/my_scripts.js
+++ d2g_sandbox/js/my_scripts.js
@@ -12,20 +12,7 @@ $(function () {
 		return false;
 	});
 	
-	//script for tabs
-	$("div.selectTabs").each(function () {
-		var tmp = $(this);
-		$(tmp).find(".lineTabs li").each(function (i) {
-			$(tmp).find(".lineTabs li:eq("+i+") a").click(function(){
-				var tab_id=i+1;
-				$(tmp).find(".lineTabs li").removeClass("active");
-				$(this).parent().addClass("active");
-				$(tmp).find(".tab_content div").stop(false,false).hide();
-				$(tmp).find(".tab"+tab_id).stop(false,false).fadeIn(300);
-				return false;
-			});
-		});
-	});
+
 });	
 
 
